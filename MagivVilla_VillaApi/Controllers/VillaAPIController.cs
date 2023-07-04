f7cf31b95b27723b90e7ac629352ad8046bdce1c
ZvalsoraY <yazamail@yandex.ru>
2023-07-04 18:34:52 +0300

    com1

--- MagivVilla_VillaApi/Controllers/VillaAPIController.cs
+++ MagivVilla_VillaApi/Controllers/VillaAPIController.cs
@@ -15,5 +15,11 @@ namespace MagivVilla_VillaApi.Controllers
         {
             return VillaStore.villaList;
         }
+        [HttpGet("id:int")]
+        public VillaDTO GetVillas(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        }
+
     }
 }
